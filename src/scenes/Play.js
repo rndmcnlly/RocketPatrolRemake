ea0fe6bf297c19154c67506ecd33405fa64c83ff
Adam Smith <adam@adamsmith.as>
2021-04-07 14:27:34 -0700

    Made the starfield background scroll infinitely!!!!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -27,8 +27,9 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    }
 
-        
-
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
