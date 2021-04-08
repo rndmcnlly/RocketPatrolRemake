67d2d9c75305084022f38f9901a998998dc92df6
Adam Smith <adam@adamsmith.as>
2021-04-08 09:59:35 -0700

    working fire controls and rocket reset

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,15 +3,34 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture);
         scene.add.existing(this);
         this.movementSpeed = 2;
+        this.firingSpeed = 4;
+        this.isFiring = false;
     }
 
     update() {
-        if(keyLEFT.isDown) {
-            this.x -= this.movementSpeed;
+        
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
         }
-        if(keyRIGHT.isDown) {
-            this.x += this.movementSpeed;
+
+        if(this.isFiring) {
+
+            this.y -= this.firingSpeed;
+            if(this.y < 108) {
+                this.isFiring = false;
+                this.y = game.config.height - borderUISize - borderPadding
+            }
+
+        } else {
+
+            if(keyLEFT.isDown) {
+                this.x -= this.movementSpeed;
+            }
+            if(keyRIGHT.isDown) {
+                this.x += this.movementSpeed;
+            }
+
+            this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width - borderUISize - borderPadding);
         }
-        this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width - borderUISize - borderPadding);
     }
 }
