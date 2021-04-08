3363fca53fc08aea479591c854f5cd48678329a6
Adam Smith <adam@adamsmith.as>
2021-04-08 09:53:52 -0700

    working keyboard controls

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,5 +2,16 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         scene.add.existing(this);
+        this.movementSpeed = 2;
+    }
+
+    update() {
+        if(keyLEFT.isDown) {
+            this.x -= this.movementSpeed;
+        }
+        if(keyRIGHT.isDown) {
+            this.x += this.movementSpeed;
+        }
+        this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width - borderUISize - borderPadding);
     }
 }
